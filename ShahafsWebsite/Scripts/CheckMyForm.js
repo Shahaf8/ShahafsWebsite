478c0bcf9c0112f9b57b1cd61bf1e7f3019d7b18
Lab314 <Lab314@314-CS01>
2023-01-18 16:18:07 +0200

    added hobies. java script not ok.

--- ShahafsWebsite/Scripts/CheckMyForm.js
+++ ShahafsWebsite/Scripts/CheckMyForm.js
@@ -1,4 +1,13 @@
 ﻿function chkForm() {
+
+    //--- בדיקת שם משתמש ---
+    var uName = document.getElementById("uName").value;
+    //alert("uName = " + uName);
+    if (!userNameOK(uName))
+        return false;
+
+
+
     var hobies = document.getElementsByName("hobies");
     var hobChecked = false;
     for (var i = 0; i < hobies.length; i++) {
@@ -16,43 +25,43 @@
         document.getElementById("mHobies").style.display = "none";
 }
 
-    function userNameOK(name) {
-        var msg = "";
+function userNameOK(name) {
+    var msg = "";
 
-        if (name.length < 6)
-            msg = "שם משתמש קצר מדי או לא קיים ";
-        else if (name.length > 30)
-            msg = "שם משתמש יהיה בין 6-30 תווים";
-        else if (isHebrew(name))
-            msg = "שם משתמש אינו יכול להכיל תווים בעברית";
+    if (name.length < 6)
+        msg = "שם משתמש קצר מדי או לא קיים ";
+    else if (name.length > 30)
+        msg = "שם משתמש יהיה בין 6-30 תווים";
+    else if (isHebrew(name))
+        msg = "שם משתמש אינו יכול להכיל תווים בעברית";
 
-        if (msg != "") {
-            document.getElementById("mUName").value = msg;
-            document.getElementById("mUName").style.display = "inline";
-            return false;
-        }
-        else
-            document.getElementById("mUName").style.display = "none";
-        if (isBadChars(name))
-            msg = "שם משתמש יכול להכיל אותיות ו/או ספרות בלבד"
-        return true;
-    }
-    //-- מכיל תווים אסורים--
-    function isBadChars(str) {
-        var badChr = "$%^&*()! []{}<>?"
-        var len = badChr.length;
-
-        var i = 0, pos, ch;
-        while (i < len) {
-            ch = badChr.charAt(i);
-            pos = str.indexOf(ch);
-            if (pos != -1) {
-                return true
-            }
-            i++
-        }
+    if (msg != "") {
+        document.getElementById("mUName").value = msg;
+        document.getElementById("mUName").style.display = "inline";
         return false;
     }
+    else
+        document.getElementById("mUName").style.display = "none";
+    if (isBadChars(name))
+        msg = "שם משתמש יכול להכיל אותיות ו/או ספרות בלבד"
+    return true;
+}
+//-- מכיל תווים אסורים--
+function isBadChars(str) {
+    var badChr = "$%^&*()! []{}<>?"
+    var len = badChr.length;
+
+    var i = 0, pos, ch;
+    while (i < len) {
+        ch = badChr.charAt(i);
+        pos = str.indexOf(ch);
+        if (pos != -1) {
+            return true
+        }
+        i++
+    }
+    return false;
+}
 
 
 
